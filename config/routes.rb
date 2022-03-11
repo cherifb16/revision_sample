510b24d856ddfe13b35b1d9491858e4f64928c51
cherifb16 <cherifbobbyray@gmail.com>
2022-03-11 13:14:06 +0200

    rails updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
-  get '/blogs', to: 'blogs#index' 
+  resources :books
+  # get '/blogs', to: 'blogs#index' 
   get '/stocks', to: 'stocks#index' 
   resources :students
   resources :posts
+  resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_04_124504) do
+ActiveRecord::Schema.define(version: 2022_03_09_121919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2022_03_04_124504) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.text "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
